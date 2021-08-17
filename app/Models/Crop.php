1ed4e2c89b8cad7ec254fe808669e8b260160879
Dalpat Singh <dalpatsingh@innowaysit.com>
2021-08-17 20:34:44 +0530

    first commit

---
+++ app/Models/Crop.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Crop extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'photo',
+        'price',
+        'unit',
+        'status',
+    ];
+
+    public function farmer()
+    {
+        return $this->belongsTo(User::class,'user_id');
+    }
+}

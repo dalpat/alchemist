1d6eef5e6245520f2e571fbd6f42be611069b715
Dalpat Singh <dalpatsingh@innowaysit.com>
2021-08-17 20:46:29 +0530

    removed farmer middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,7 +3,6 @@
 namespace App\Http;
 
 use App\Http\Middleware\AdminMiddleware;
-use App\Http\Middleware\FarmerMiddleware;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -65,6 +64,5 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'admin'=> AdminMiddleware::class,
-        'farmer'=> FarmerMiddleware::class,
     ];
 }
